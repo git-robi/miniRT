c90fa45865b666ceddf51f09f66135f5e3391caa
Roberta Giambona <giambonaroberta0@gmail.com>
2025-02-12 15:15:00 +0100

    feat: ft_isfloat function

--- float_checks.c
+++ float_checks.c
@@ -1,7 +1,34 @@
 bool	is_floatoverflow(char *str)
 {
+
 }
 
-bool	ft_isfloat(char *str)
+bool ft_isfloat(const char *str)
 {
+	int	i;
+	int	has_dot;
+	int	has_digit;
+
+	i = 0;
+	has_dot = 0;
+	has_digit = 0;
+	while (isspace(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (isdigit(str[i]))
+			has_digit = 1;
+		else if (str[i] == '.')
+		{
+			if (has_dot)
+				return 0;
+			has_dot = 1;
+		}
+		else
+			return 0;
+		i++;
+	}
+	return (has_digit);
 }
