8dfa91de3ac8912f0e57238833140981229dc158
Taha Tahere <elberkani.taha@gmail.com>
2025-02-12 12:01:22 +0100

    moved a structure.

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,21 +6,24 @@
 /*   By: tatahere <tatahere@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 11:28:41 by tatahere          #+#    #+#             */
-/*   Updated: 2025/02/12 11:51:18 by tatahere         ###   ########.fr       */
+/*   Updated: 2025/02/12 12:00:50 by tatahere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef	struct s_camera
-{
+#ifndef MINIRT_H
+# define MINIRT_H
 
+typedef struct s_camera
+{
+	t_coordinates origin;
+	t_coordinates direction;
+	t_coordinates fov;
 }	t_camera;
-
-typedef struct s_sphere
-{
-}	t_sphere;
 	
 typedef	struct s_cene
 {
 	t_camera	camera;
 	t_list		*spheres;
 }
+
+#endif
