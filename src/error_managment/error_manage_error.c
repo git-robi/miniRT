51d6219d4aa4fc39336e0c4212846e6fba524999
Taha Tahere <elberkani.taha@gmail.com>
2025-02-12 15:39:07 +0100

    added structure and function to manage errors

---
+++ src/error_managment/error_manage_error.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_manage_error.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tatahere <tatahere@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 15:25:45 by tatahere          #+#    #+#             */
+/*   Updated: 2025/02/12 15:36:37 by tatahere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+#include "error_managment.h"
+
+void	error_manage_error(t_error *error)
+{
+	ft_putstr_fd(error->msg, 2);
+	if (error->is_msg_freeable)
+		free(msg);
+	exit(error->exit_status);
+}
