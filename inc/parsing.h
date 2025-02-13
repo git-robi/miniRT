7162bc928c39e80937e741d10e07d804e9fb9543
Roberta Giambona <rgiambon@cbr12s5.42barcelona.com>
2025-02-13 13:26:00 +0100

    add: parsing prototypes

--- inc/parsing.h
+++ inc/parsing.h
@@ -6,7 +6,7 @@
 /*   By: tatahere <tatahere@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 17:50:57 by tatahere          #+#    #+#             */
-/*   Updated: 2025/02/13 10:50:24 by tatahere         ###   ########.fr       */
+/*   Updated: 2025/02/13 13:25:44 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 # define PARSING_H
 
 void	parse_file(t_error *error, t_cene *cene, int ac, char **av);
+
+
+t_sphere	*parse_sphere(char **tokens, t_scene *scene);
+t_light		parse_light(char **tokens, t_scene *scene);
+t_camera	parse_camera(char **tokens, t_scene *scene);
 
 #endif
