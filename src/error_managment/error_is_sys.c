94774366ba51f0379169ca94383ff670f1321511
Roberta Giambona <rgiambon@cbr10s4.42barcelona.com>
2025-03-20 14:22:47 +0100

    fix(error_managment): norm

--- src/error_managment/error_is_sys.c
+++ src/error_managment/error_is_sys.c
@@ -6,7 +6,7 @@
 /*   By: tatahere <tatahere@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:42:18 by tatahere          #+#    #+#             */
-/*   Updated: 2025/02/14 16:36:40 by tatahere         ###   ########.fr       */
+/*   Updated: 2025/03/20 14:21:23 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "error_managment.h"
 #include "custom_errors.h"
 
-int		error_is_sys(t_error *error)
+int	error_is_sys(t_error *error)
 {
 	if (error->errnum < LAST_SYS_ERROR && error->errnum != 0)
 		return (1);
--- src/error_managment/error_managment_local.h
+++ src/error_managment/error_managment_local.h
@@ -6,14 +6,14 @@
 /*   By: tatahere <tatahere@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:06:05 by tatahere          #+#    #+#             */
-/*   Updated: 2025/02/17 15:13:50 by tatahere         ###   ########.fr       */
+/*   Updated: 2025/03/20 14:22:20 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_MANAGMENT_LOCAL_H
 # define ERROR_MANAGMENT_LOCAL_H
 
-#include "error_managment.h"
+# include "error_managment.h"
 
 void	error_msg_destroy(t_error *error);
 
